2360719a224debb1c2f1a358dcb4e452a2bdf750
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2024-12-22 23:36:13 +0530

    Add NoteController and UserController for managing notes and users

---
+++ backend/NoteApp/src/main/java/com/devProject/NoteApp/controller/NoteController.java
@@ -0,0 +1,45 @@
+package com.devProject.NoteApp.controller;
+
+import com.devProject.NoteApp.model.Note;
+import com.devProject.NoteApp.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+    @Autowired
+    private NoteService noteService;
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Note> getNotesByUserId(@PathVariable String userId) {
+        return noteService.getNotesByUserId(userId);
+    }
+
+    @GetMapping("/{id}")
+    public Note getNoteById(@PathVariable String id) {
+        return noteService.getNoteById(id);
+    }
+
+    @PostMapping
+    public Note createNote(@RequestBody Note note) {
+        return noteService.createNote(note);
+    }
+
+    @PutMapping("/{id}")
+    public Note updateNote(@PathVariable String id, @RequestBody Note note) {
+        return noteService.updateNote(id, note);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable String id) {
+        noteService.deleteNote(id);
+    }
+}
