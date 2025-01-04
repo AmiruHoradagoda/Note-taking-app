6e5ef1000a91cbbcfba66a5bdc94eba68e0b8431
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-05 01:52:24 +0530

    Add DTO classes for authentication and registration requests

---
+++ backend/NoteApp/src/main/java/com/devProject/NoteApp/dto/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.devProject.NoteApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+}
