8e0cbb3ba06d9be19a7b0cc13b6a497013578010
AmiruHoradagoda <139224955+AmiruHoradagoda@users.noreply.github.com>
2025-01-05 01:51:17 +0530

    Replace UserRepository with UserRepo for improved user data access

--- backend/NoteApp/src/main/java/com/devProject/NoteApp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devProject.NoteApp.repository;
-
-import com.devProject.NoteApp.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
